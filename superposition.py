ef954e1e033860d6bb60af4c1b72d419a2433670
markozeman <mz4730@student.uni-lj.si>
2020-02-20 16:19:09 +0100

    Real superposition added

--- superposition.py
+++ superposition.py
@@ -9,6 +9,7 @@ from keras.utils.np_utils import to_categorical
 from keras.optimizers import Adam
 from keras.callbacks import Callback, LearningRateScheduler
 from plots import *
+from functools import reduce
 from help_functions import *
 from math import exp
 import numpy as np
@@ -41,8 +42,8 @@ class TestSuperpositionPerformanceCallback(Callback):
         self.y_test = y_test
         self.context_matrices = context_matrices
         self.model = model
-        self.accuracies = []
         self.task_index = task_index
+        self.accuracies = []
 
     def on_epoch_begin(self, epoch, logs=None):
         if self.task_index == 0:    # first task (original MNIST images) - we did not use context yet
@@ -52,13 +53,13 @@ class TestSuperpositionPerformanceCallback(Callback):
         # save current model weights (without bias node)
         curr_w_matrices = []
         curr_bias_vectors = []
-        for _, layer in enumerate(self.model.layers[1:]):  # first layer is Flatten so we skip it
+        for layer in self.model.layers[1:]:  # first layer is Flatten so we skip it
             curr_w_matrices.append(layer.get_weights()[0])
             curr_bias_vectors.append(layer.get_weights()[1])
 
         # temporarily change model weights to be suitable for first task (original MNIST images), (without bias node)
         for i, layer in enumerate(self.model.layers[1:]):  # first layer is Flatten so we skip it
-            # multiplying with inverse matrices to 'unfold'
+            # # multiplying with inverse matrices to 'unfold'
             # context_inverse_multiplied = np.linalg.inv(self.context_matrices[self.task_index][i])
             # for task_i in range(self.task_index - 1, 0, -1):
             #     context_inverse_multiplied = context_inverse_multiplied @ np.linalg.inv(self.context_matrices[task_i][i])
@@ -80,6 +81,46 @@ class TestSuperpositionPerformanceCallback(Callback):
         # change model weights back (without bias node)
         for i, layer in enumerate(self.model.layers[1:]):  # first layer is Flatten so we skip it
             layer.set_weights([curr_w_matrices[i], curr_bias_vectors[i]])
+
+
+class TestRealSuperpositionPerformanceCallback(Callback):
+    """
+    Callback class for testing real superposition model performance at the beginning of every epoch.
+    """
+    def __init__(self, X_test, y_test, context_matrices, model, saved_weights):
+        super().__init__()
+        self.X_test = X_test
+        self.y_test = y_test
+        self.context_matrices = context_matrices
+        self.model = model
+        self.saved_weights = saved_weights
+        self.accuracies = []
+
+    def on_epoch_begin(self, epoch, logs=None):
+        # get current model weights and save them to later restore the model
+        curr_weights = [layer.get_weights() for layer in self.model.layers[1:]]
+
+        # first, element-wise sum up model weights by layers from the list self.saved_weights
+        # second, sum up current model weights and summed up weights from self.saved_weights
+        if len(self.saved_weights) == 0:
+            summed_weights = curr_weights
+        elif len(self.saved_weights) == 1:
+            summed_weights = sum_up_weights([self.saved_weights[0], curr_weights])
+        else:   # len(self.saved_weights) >= 2
+            summed_weights = sum_up_weights([sum_up_weights(self.saved_weights), curr_weights])
+
+        # multiply with inverse matrix of the first context matrix (without changing bias node), set that as temporary model weights
+        for i, layer in enumerate(self.model.layers[1:]):
+            # context matrix should be inversed but in this case of binary superposition the matrix inverse is the same as original
+            layer.set_weights([summed_weights[i][0] @ self.context_matrices[0][i], summed_weights[i][1]])
+
+        # evaluate only on 1.000 images (10% of all test images) to speed-up
+        loss, accuracy = self.model.evaluate(self.X_test[:1000], self.y_test[:1000], verbose=2)
+        self.accuracies.append(accuracy * 100)
+
+        # change model weights back (restore the model)
+        for index, layer in enumerate(self.model.layers[1:]):
+            layer.set_weights(curr_weights[index])
 
 
 lr_over_time = []   # global variable to store changing learning rates
@@ -106,6 +147,20 @@ def lr_scheduler(epoch, lr):
     return max(lr, 0.000001)    # don't let learning rate go to 0
 
 
+def sum_up_weights(weights_list):
+    """
+    Element-wise sum up model weights (with biases) by layers.
+    Function is suitable only for networks with 3 weights matrices.
+
+    :param weights_list: list of all model weights with biases (len(weights_list) should be >= 2)
+    :return: model weights with biases
+    """
+    return reduce(lambda x, y: [[np.add(x[0][0], y[0][0]), np.add(x[0][1], y[0][1])],
+                                [np.add(x[1][0], y[1][0]), np.add(x[1][1], y[1][1])],
+                                [np.add(x[2][0], y[2][0]), np.add(x[2][1], y[2][1])]],
+            weights_list)
+
+
 def simple_model(input_size, num_of_units, num_of_classes):
     """
     Create simple NN model with two hidden layers, each has 'num_of_units' neurons.
@@ -126,7 +181,7 @@ def simple_model(input_size, num_of_units, num_of_classes):
 
 
 def train_model(model, X_train, y_train, X_test, y_test, num_of_epochs, batch_size=32, validation_share=0.0,
-                superposition=False, context_matrices=None, task_index=None):
+                mode='normal', context_matrices=None, task_index=None, saved_weights=None):
     """
     Train and evaluate Keras model.
 
@@ -138,18 +193,48 @@ def train_model(model, X_train, y_train, X_test, y_test, num_of_epochs, batch_si
     :param num_of_epochs: number of epochs to train the model
     :param batch_size: batch size - number of samples per gradient update (default = 32)
     :param validation_share: share of examples to be used for validation (default = 0)
-    :param superposition: boolean value if we train with superposition - important for callback
-    :param context_matrices: multidimensional numpy array with random context (binary superposition), only used when superposition = True
-    :param task_index: index of current task, only used when superposition = True
-    :return: History object and two lists of test accuracies for every training epoch (normal and superposition)
+    :param mode: string for learning mode, important for callbacks - possible values: 'normal', 'superposition', 'real superposition'
+    :param context_matrices: multidimensional numpy array with random context (binary superposition), only used when mode = 'superposition'
+    :param task_index: index of current task, only used when mode = 'superposition'
+    :param saved_weights: weights of the model at the end of each task, only used when mode = 'real superposition'
+    :return: History object and 3 lists of test accuracies for every training epoch (normal, superposition and real superposition)
     """
     test_callback = TestPerformanceCallback(X_test, y_test)
     test_superposition_callback = TestSuperpositionPerformanceCallback(X_test, y_test, context_matrices, model, task_index)
+    test_real_superposition_callback = TestRealSuperpositionPerformanceCallback(X_test, y_test, context_matrices, model, saved_weights)
     lr_callback = LearningRateScheduler(lr_scheduler)
-    callbacks = [test_superposition_callback, lr_callback] if superposition else [test_callback, lr_callback]
+
+    callbacks = [lr_callback]
+    if mode == 'normal':
+        callbacks.append(test_callback)
+    elif mode == 'superposition':
+        callbacks.append(test_superposition_callback)
+    elif mode == 'real superposition':
+        callbacks.append(test_real_superposition_callback)
+
     history = model.fit(X_train, y_train, epochs=num_of_epochs, batch_size=batch_size, verbose=2,
                         validation_split=validation_share, callbacks=callbacks)
-    return history, test_callback.accuracies, test_superposition_callback.accuracies
+    return history, test_callback.accuracies, test_superposition_callback.accuracies, test_real_superposition_callback.accuracies
+
+
+def prepare_data(num_of_classes):
+    """
+    Normalize and prepare MNIST data to be ready for NN input.
+
+    :param num_of_classes: number of output classes/labels
+    :return:  X_train, y_train, X_test, y_test
+    """
+    X_train, y_train, X_test, y_test = get_MNIST()
+    y_train = to_categorical(y_train, num_classes=num_of_classes)  # one-hot encode
+    y_test = to_categorical(y_test, num_classes=num_of_classes)  # one-hot encode
+
+    # normalize input images to have values between 0 and 1
+    X_train = X_train.astype(dtype=np.float64)
+    X_test = X_test.astype(dtype=np.float64)
+    X_train /= 255
+    X_test /= 255
+
+    return X_train, y_train, X_test, y_test
 
 
 def permute_pixels(im, seed):
@@ -229,7 +314,7 @@ def normal_training(model, X_train, y_train, X_test, y_test, num_of_epochs, num_
     original_accuracies = []
 
     # first training task - original MNIST images
-    history, accuracies, _ = train_model(model, X_train, y_train, X_test, y_test, num_of_epochs, batch_size, validation_share=0.1)
+    history, accuracies, _, _ = train_model(model, X_train, y_train, X_test, y_test, num_of_epochs, batch_size, validation_share=0.1)
     original_accuracies.extend(accuracies)
 
     val_acc = np.array(history.history['val_accuracy']) * 100
@@ -239,13 +324,12 @@ def normal_training(model, X_train, y_train, X_test, y_test, num_of_epochs, num_
     for i in range(num_of_tasks - 1):
         print("\n\n i: %d \n" % i)
         permuted_X_train = permute_images(X_train)
-        history, accuracies, _ = train_model(model, permuted_X_train, y_train, X_test, y_test, num_of_epochs, batch_size, validation_share=0.1)
+        history, accuracies, _, _ = train_model(model, permuted_X_train, y_train, X_test, y_test, num_of_epochs, batch_size, validation_share=0.1)
         original_accuracies.extend(accuracies)
 
         val_acc = np.array(history.history['val_accuracy']) * 100
         print('\nValidation accuracies: ', i, val_acc)
 
-    print('original_accuracies', len(original_accuracies), original_accuracies)
     return original_accuracies
 
 
@@ -281,7 +365,7 @@ def superposition_training(model, X_train, y_train, X_test, y_test, num_of_epoch
     :param num_of_classes: number of different classes/output labels
     :param num_of_tasks: number of different tasks (permutations of original images)
     :param batch_size: batch size - number of samples per gradient update (default = 32)
-    :return: list of test accuracies for 10 epochs for each task (for original task validation accuracies)
+    :return: list of test accuracies for 10 epochs for each task (or validation accuracies for original task)
     """
     original_accuracies = []
     context_matrices = get_context_matrices(num_of_units, num_of_classes, num_of_tasks)
@@ -290,8 +374,8 @@ def superposition_training(model, X_train, y_train, X_test, y_test, num_of_epoch
     # context_multiplication(model, context_matrices, 0)
 
     # first training task - original MNIST images
-    history, _, _ = train_model(model, X_train, y_train, X_test, y_test, num_of_epochs, batch_size,
-                                         validation_share=0.1, superposition=True, context_matrices=context_matrices, task_index=0)
+    history, _, _, _ = train_model(model, X_train, y_train, X_test, y_test, num_of_epochs, batch_size, validation_share=0.1,
+                                   mode='superposition', context_matrices=context_matrices, task_index=0)
 
     val_acc = np.array(history.history['val_accuracy']) * 100
     print('\nValidation accuracies: ', 'first', val_acc)
@@ -306,14 +390,70 @@ def superposition_training(model, X_train, y_train, X_test, y_test, num_of_epoch
         context_multiplication(model, context_matrices, i + 1)
 
         permuted_X_train = permute_images(X_train)
-        history, _, accuracies = train_model(model, permuted_X_train, y_train, X_test, y_test, num_of_epochs, batch_size,
-                                             validation_share=0.1, superposition=True, context_matrices=context_matrices, task_index=i + 1)
+        history, _, accuracies, _ = train_model(model, permuted_X_train, y_train, X_test, y_test, num_of_epochs, batch_size, validation_share=0.1,
+                                                mode='superposition', context_matrices=context_matrices, task_index=i + 1)
         original_accuracies.extend(accuracies)
 
         val_acc = np.array(history.history['val_accuracy']) * 100
         print('\nValidation accuracies: ', i, val_acc)
 
-    print('original_accuracies', len(original_accuracies), original_accuracies)
+    return original_accuracies
+
+
+def real_superposition_training(model, X_train, y_train, X_test, y_test, num_of_epochs, num_of_units, num_of_classes, num_of_tasks, batch_size=32):
+    """
+    Train model for 'num_of_tasks' tasks, each task is a different permutation of input images.
+    Check how accuracy for original images is changing through tasks using real superposition training.
+
+    :param model: Keras model instance
+    :param X_train: train input data
+    :param y_train: train output labels
+    :param X_test: test input data
+    :param y_test: test output labels
+    :param num_of_epochs: number of epochs to train the model
+    :param num_of_units: number of neurons in each hidden layer
+    :param num_of_classes: number of different classes/output labels
+    :param num_of_tasks: number of different tasks (permutations of original images)
+    :param batch_size: batch size - number of samples per gradient update (default = 32)
+    :return: list of test accuracies for 10 epochs for each task
+    """
+    init_weights = [layer.get_weights() for layer in model.layers[1:]]   # first layer is Flatten so we skip it
+
+    original_accuracies = []
+    saved_weights = []   # saved weights at the end of each task
+    context_matrices = get_context_matrices(num_of_units, num_of_classes, num_of_tasks)
+
+    # multiply random initialized weights with context matrices for each layer (without changing weights from bias node)
+    context_multiplication(model, context_matrices, 0)
+
+    history, _, _, accuracies = train_model(model, X_train, y_train, X_test, y_test, num_of_epochs, batch_size, validation_share=0.1,
+                                            mode='real superposition', context_matrices=context_matrices, saved_weights=saved_weights)
+    original_accuracies.extend(accuracies)
+    saved_weights.append([layer.get_weights() for layer in model.layers[1:]])   # save weights at the end of training
+
+    val_acc = np.array(history.history['val_accuracy']) * 100
+    print('\nValidation accuracies: ', 'first', val_acc)
+
+    # other training tasks - permuted MNIST data
+    for i in range(num_of_tasks - 1):
+        print("\n\n i: %d \n" % i)
+
+        # set model weights to the same weights as the first model was initialized to
+        for index, layer in enumerate(model.layers[1:]):
+            layer.set_weights(init_weights[index])
+
+        # multiply current weights with context matrices for each layer (without changing weights from bias node)
+        context_multiplication(model, context_matrices, i + 1)
+
+        permuted_X_train = permute_images(X_train)
+        history, _, _, accuracies = train_model(model, permuted_X_train, y_train, X_test, y_test, num_of_epochs, batch_size, validation_share=0.1,
+                                                mode='real superposition', context_matrices=context_matrices, saved_weights=saved_weights)
+        original_accuracies.extend(accuracies)
+        saved_weights.append([layer.get_weights() for layer in model.layers[1:]])  # save weights at the end of training
+
+        val_acc = np.array(history.history['val_accuracy']) * 100
+        print('\nValidation accuracies: ', i, val_acc)
+
     return original_accuracies
 
 
@@ -322,23 +462,16 @@ if __name__ == '__main__':
     num_of_units = 1024
     num_of_classes = 10
 
-    num_of_tasks = 4       # todo - change to 50
+    num_of_tasks = 10       # todo - change to 50
     num_of_epochs = 10
     batch_size = 600
 
     train_normal = False
-    train_superposition = True
+    train_superposition = False
+    train_real_superposition = True
 
     if train_normal:
-        X_train, y_train, X_test, y_test = get_MNIST()
-        y_train = to_categorical(y_train, num_classes=num_of_classes)   # one-hot encode
-        y_test = to_categorical(y_test, num_classes=num_of_classes)     # one-hot encode
-
-        # normalize input images to have values between 0 and 1
-        X_train = X_train.astype(dtype=np.float64)
-        X_test = X_test.astype(dtype=np.float64)
-        X_train /= 255
-        X_test /= 255
+        X_train, y_train, X_test, y_test = prepare_data(num_of_classes)
 
         model = simple_model(input_size, num_of_units, num_of_classes)
 
@@ -350,15 +483,7 @@ if __name__ == '__main__':
 
     if train_superposition:
         lr_over_time = []  # re-initiate learning rate
-        X_train, y_train, X_test, y_test = get_MNIST()
-        y_train = to_categorical(y_train, num_classes=num_of_classes)  # one-hot encode
-        y_test = to_categorical(y_test, num_classes=num_of_classes)  # one-hot encode
-
-        # normalize input images to have values between 0 and 1
-        X_train = X_train.astype(dtype=np.float64)
-        X_test = X_test.astype(dtype=np.float64)
-        X_train /= 255
-        X_test /= 255
+        X_train, y_train, X_test, y_test = prepare_data(num_of_classes)
 
         model = simple_model(input_size, num_of_units, num_of_classes)
 
@@ -372,8 +497,15 @@ if __name__ == '__main__':
             plot_accuracies_over_time(acc_normal, acc_superposition)
 
 
+    if train_real_superposition:
+        lr_over_time = []  # re-initiate learning rate
+        X_train, y_train, X_test, y_test = prepare_data(num_of_classes)
 
-    # todo
-    # make bullet-points of work done
-    # superposition on blackboard
+        model = simple_model(input_size, num_of_units, num_of_classes)
+
+        acc_real_superposition = real_superposition_training(model, X_train, y_train, X_test, y_test, num_of_epochs,
+                                                             num_of_units, num_of_classes, num_of_tasks, batch_size)
+
+        plot_lr(lr_over_time)
+        plot_accuracies_over_time(np.zeros(len(acc_real_superposition)), acc_real_superposition)
 
